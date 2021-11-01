e07b4a9718794dffbbc57c53178a4ad92e273b8b
Dave McAlpine <dave@mcalpine.io>
2021-10-31 21:53:35 -0400

    Added a Stack implementation backed by a linked-list
    The StackLL struckt is a stack (LI-FO) data structure which uses a
    linked-list of StackNode as its underlying collection mechanism.

--- data_structures/stack_linked_list.go
+++ data_structures/stack_linked_list.go
@@ -1,1 +1,64 @@
 package data_structures
+
+// A stack is a collection of
+// data where the elements are
+// operated on in a last-in, first-out
+// manner. This stack uses a linked-list
+// as it's underlying data structure.
+
+type StackLL struct {
+	size int
+	head *StackNode
+}
+
+type StackNode struct {
+	value interface{}
+	next  *StackNode
+}
+
+// Returns a new StackLL
+func NewStackLL() *StackLL {
+	return &StackLL{}
+}
+
+// Adds a new item to the stack
+func (s *StackLL) Push(item interface{}) {
+	s.size++
+
+	node := &StackNode{
+		value: item,
+		next:  s.head,
+	}
+
+	s.head = node
+}
+
+// Removes and returns the last
+// item added to the stack
+func (s *StackLL) Pop() interface{} {
+	if s.head == nil {
+		return nil
+	}
+
+	s.size--
+	item := s.head
+	s.head = s.head.next
+
+	return item.value
+}
+
+// Only returns the last item
+// in the stack
+func (s *StackLL) Peek() interface{} {
+	if s.head != nil {
+		return s.head.value
+	}
+
+	return nil
+}
+
+// Returns true if there are no
+// itmes in the stack
+func (s *StackLL) IsEmpty() bool {
+	return s.size == 0
+}
--- data_structures/stack_linked_list_test.go
+++ data_structures/stack_linked_list_test.go
@@ -1,1 +1,118 @@
 package data_structures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type StackLLSuite struct {
+	suite.Suite
+}
+
+func (s *StackLLSuite) TestNewStackLL() {
+	stack := NewStackLL()
+	assert.Equal(s.T(), &StackLL{}, stack)
+}
+
+func (s *StackLLSuite) TestPush() {
+	stack := NewStackLL()
+
+	stack.Push(1)
+	assert.Equal(s.T(), 1, stack.head.value)
+
+	stack.Push(2)
+	assert.Equal(s.T(), 2, stack.head.value)
+
+	stack.Push(3)
+	assert.Equal(s.T(), 3, stack.head.value)
+}
+
+func (s *StackLLSuite) TestPop() {
+	stack := NewStackLL()
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+
+	item := stack.Pop()
+	assert.Equal(s.T(), 3, item)
+
+	item = stack.Pop()
+	assert.Equal(s.T(), 2, item)
+
+	item = stack.Pop()
+	assert.Equal(s.T(), 1, item)
+
+	item = stack.Pop()
+	assert.Equal(s.T(), nil, item)
+}
+
+func (s *StackLLSuite) TestPeek() {
+	stack := NewStackLL()
+	stack.Push(2)
+	stack.Push(3)
+
+	item := stack.Peek()
+	assert.Equal(s.T(), 3, item)
+
+	item = stack.Peek()
+	assert.Equal(s.T(), 3, item)
+
+	stack.Pop()
+
+	item = stack.Peek()
+	assert.Equal(s.T(), 2, item)
+}
+
+func (s *StackLLSuite) TestIsEmpty() {
+	stack := NewStackLL()
+	stack.Push(1)
+	assert.False(s.T(), stack.IsEmpty())
+
+	stack.Pop()
+	assert.True(s.T(), stack.IsEmpty())
+}
+
+func BenchmarkStackLLPush(t *testing.B) {
+	stack := NewStackLL()
+
+	for i := 0; i < t.N; i++ {
+		stack.Push(i)
+	}
+}
+
+func BenchmarkStackLLPop(t *testing.B) {
+	stack := NewStackLL()
+	for i := 0; i < 100000; i++ {
+		stack.Push(i)
+	}
+
+	for i := 0; i < t.N; i++ {
+		stack.Pop()
+	}
+}
+
+func BenchmarkStackLLPeek(t *testing.B) {
+	stack := NewStackLL()
+	for i := 0; i < 1000; i++ {
+		stack.Push(i)
+	}
+
+	for i := 0; i < t.N; i++ {
+		stack.Peek()
+	}
+}
+
+func BenchmarkIsStackLLEmpty(t *testing.B) {
+	stack := NewStackLL()
+	stack.Push(1)
+
+	for i := 0; i < t.N; i++ {
+		stack.IsEmpty()
+	}
+}
+
+func TestStackLLSuite(t *testing.T) {
+	suite.Run(t, new(StackLLSuite))
+}
