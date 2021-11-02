9c022e3fc50b475ec8ea783c3f3782eb8332d9ec
Dave McAlpine <dave@mcalpine.io>
2021-11-01 21:53:39 -0400

    Added a map to map.go
    This data structure is an associate array. It has the ability to Set,
    Get and Delete data.

--- data_structures/map.go
+++ data_structures/map.go
@@ -1,1 +1,67 @@
 package data_structures
+
+// A Map is a an associative array which
+// means values are stored and retrieved
+// along with keys which identify them
+
+type Map struct {
+	items []*MapNode
+}
+
+type MapNode struct {
+	key   interface{}
+	value interface{}
+}
+
+// Instantiates a new Map
+func NewMap() *Map {
+	return &Map{}
+}
+
+// Sets an item at key: k to value: v
+// If the key already exists, its value
+// will be over-written
+func (m *Map) Set(k interface{}, v interface{}) {
+	_, node := m.findMapNode(k)
+	if node != nil {
+		node.value = v
+		return
+	}
+
+	m.items = append(m.items, &MapNode{k, v})
+}
+
+// Retrieves an item from the map located
+// at key: k
+func (m *Map) Get(k interface{}) interface{} {
+	_, node := m.findMapNode(k)
+	if node == nil {
+		return nil
+	}
+
+	return node.value
+}
+
+// Deletes an item from the map located at
+// key: k
+func (m *Map) Delete(k interface{}) {
+	i, node := m.findMapNode(k)
+	if node == nil {
+		return
+	}
+
+	m.items[i] = m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+}
+
+// returns the index and item belonging to key: k
+// if no key is found, the index will be -1
+// and node will be nil
+func (m *Map) findMapNode(k interface{}) (int, *MapNode) {
+	for i, item := range m.items {
+		if item.key == k {
+			return i, item
+		}
+	}
+	return -1, nil
+}
--- data_structures/map_test.go
+++ data_structures/map_test.go
@@ -1,1 +1,43 @@
 package data_structures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type MapSuite struct {
+	suite.Suite
+}
+
+func (s *MapSuite) TestNewMap() {
+	s.Equal(&Map{}, NewMap())
+}
+
+func (s *MapSuite) TestInsertGetDelelete() {
+	mp := NewMap()
+	mp.Set("key1", "value1")
+	s.Equal("value1", mp.Get("key1"))
+
+	mp.Set("key2", 23)
+	mp.Set(3, "val 3")
+	s.Equal(23, mp.Get("key2"))
+	s.Equal("val 3", mp.Get(3))
+
+	mp.Set("key1", "value 1 updated")
+	s.Equal("value 1 updated", mp.Get("key1"))
+
+	s.Equal(3, len(mp.items))
+	mp.Delete("key1")
+	s.Equal(2, len(mp.items))
+	s.Nil(mp.Get("key1"))
+	mp.Delete(3)
+	s.Equal(1, len(mp.items))
+	s.Nil(mp.Get(3))
+	mp.Delete("doesnt_exist")
+	s.Equal(1, len(mp.items))
+}
+
+func TestMapSuite(t *testing.T) {
+	suite.Run(t, new(MapSuite))
+}
