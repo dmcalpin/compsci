e36e41238a0da10db1bc4d90bda99e4119d0fa7f
Dave McAlpine <dave@mcalpine.io>
2021-11-02 23:44:57 -0400

    Added a BinarySearch function to binary_search.go.

--- algorithms/binary_search.go
+++ algorithms/binary_search.go
@@ -1,1 +1,45 @@
 package compsci
+
+// Binary search works by cutting
+// the search in half each iteration
+// The collection must be sorted
+// first.
+//
+// The agorithm is as follows:
+// 0. left = 0, right = len(collection)
+// Loop ->
+// 1. Start in the center of left/right
+// 2. If search > iter, left = i+1
+//    continue
+// 3. If search < iter, right = i
+//    continue
+// 4. must have match, return i
+//
+// It has an efficiency of O(log n)
+
+// Returns index of found item, or -1 if not found
+func BinarySearch(collection []int, item int) int {
+	left := 0
+	right := len(collection)
+
+	for {
+		if right < left {
+			return -1
+		}
+
+		middle := (left + right) / 2
+		if middle >= len(collection) {
+			return -1
+		}
+
+		m := collection[middle]
+		if item > m {
+			left = middle + 1
+			continue
+		} else if item < m {
+			right = middle - 1
+			continue
+		}
+		return middle
+	}
+}
--- algorithms/binary_search_test.go
+++ algorithms/binary_search_test.go
@@ -1,1 +1,109 @@
 package compsci
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinarySearch(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  []int
+		search int
+		result int
+	}{
+		{
+			"empty returns -1",
+			[]int{},
+			3,
+			-1,
+		},
+		{
+			"single item with correct index",
+			[]int{3},
+			3,
+			0,
+		},
+		{
+			"single item with incorrect index",
+			[]int{3},
+			99,
+			-1,
+		},
+		{
+			"two items with first index",
+			[]int{1, 3},
+			1,
+			0,
+		},
+		{
+			"two items with first index",
+			[]int{1, 3},
+			3,
+			1,
+		},
+		{
+			"two items with no index",
+			[]int{1, 3},
+			99,
+			-1,
+		},
+		{
+			"3 items with first index",
+			[]int{1, 2, 3},
+			1,
+			0,
+		},
+		{
+			"3 items with second index",
+			[]int{1, 2, 3},
+			2,
+			1,
+		},
+		{
+			"3 items with second index",
+			[]int{1, 2, 3},
+			3,
+			2,
+		},
+		{
+			"3 items with no index",
+			[]int{1, 2, 3},
+			-1,
+			-1,
+		},
+		{
+			"many items, high index",
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 50, 60, 100, 200, 300, 400},
+			400,
+			17,
+		},
+		{
+			"many items, no index",
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 50, 60, 100, 200, 300, 400},
+			100000,
+			-1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.result, BinarySearch(tc.input, tc.search))
+		})
+	}
+}
+
+func BenchmarkBinarySearch(t *testing.B) {
+	// even at 1M items, it still takes
+	// 42 ns/op
+	numItems := 1000000
+	items := []int{}
+	for i := 0; i < numItems; i++ {
+		items = append(items, i)
+	}
+
+	for i := 0; i < t.N; i++ {
+		BinarySearch(items, i%numItems)
+	}
+}
