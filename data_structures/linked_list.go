9c63b515ba937c0fae2ea257c3a41f0b8f436e29
Dave McAlpine <dave@mcalpine.io>
2021-11-06 11:10:46 -0400

    Linked List - added basic linked list under /concepts

--- data_structures/linked_list.go
+++ data_structures/linked_list.go
@@ -1,1 +1,83 @@
 package data_structures
+
+import (
+	"fmt"
+)
+
+// A linked list is a simple data structure
+// which creates list by having each
+// node in the list maintain a reference
+// to the next one in the list
+
+type LinkedList struct {
+	// how many items are in this list
+	size int
+	// the most-recently added item
+	// in the list
+	head *Node
+}
+
+type Node struct {
+	value interface{}
+	next  *Node
+}
+
+// Returns a pointer to a LinkedList
+func NewLinkedList() *LinkedList {
+	return &LinkedList{}
+}
+
+// Inserts a new item at the head of the list
+// e.g. 2 -> 1, Insert(3), 3 -> 2 -> 1
+func (l *LinkedList) Insert(item interface{}) {
+	// create a new node who's "next" is the
+	// current head
+	n := &Node{
+		value: item,
+		next:  l.head,
+	}
+
+	// then replace the current head with the
+	// new node
+	l.head = n
+
+	// increase size
+	l.size++
+}
+
+// Removes the first matching item from the
+// list
+func (l *LinkedList) Delete(item interface{}) {
+	node := l.head
+	if node.value == item {
+		l.head = node.next
+		l.size--
+		return
+	}
+
+	for node.next != nil {
+		if node.next.value == item {
+			node.next = node.next.next
+			l.size--
+			return
+		}
+		node = node.next
+	}
+}
+
+// Returns the number of items in the list
+func (l *LinkedList) Size() int {
+	return l.size
+}
+
+// Traverses the list and prints a string
+// representation of its contents
+func (l *LinkedList) String() string {
+	repr := ""
+	node := l.head
+	for node != nil {
+		repr = fmt.Sprintf("%s -> %s", repr, node.value)
+		node = node.next
+	}
+	return repr
+}
--- data_structures/linked_list_test.go
+++ data_structures/linked_list_test.go
@@ -1,1 +1,60 @@
 package data_structures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type linkedListSuite struct {
+	suite.Suite
+
+	ll *LinkedList
+}
+
+func (s *linkedListSuite) SetupTest() {
+	s.ll = NewLinkedList()
+
+	s.ll.Insert("item 1")
+	s.ll.Insert("item 2")
+	s.ll.Insert("item 3")
+	s.ll.Insert("item 4")
+	s.ll.Insert("item 5")
+}
+
+func (s *linkedListSuite) TestNewLinkedList() {
+	s.Equal(&LinkedList{}, NewLinkedList())
+}
+
+func (s *linkedListSuite) TestInsert() {
+	ll := NewLinkedList()
+
+	ll.Insert("item 1")
+	ll.Insert("item 2")
+
+	s.Equal(" -> item 2 -> item 1", ll.String())
+	s.Equal("item 2", ll.head.value)
+	s.Equal(2, ll.Size())
+}
+
+func (s *linkedListSuite) TestDeleteMiddle() {
+	s.ll.Delete("item 3")
+	s.Equal(" -> item 5 -> item 4 -> item 2 -> item 1", s.ll.String())
+	s.Equal(4, s.ll.Size())
+}
+
+func (s *linkedListSuite) TestDeleteHead() {
+	s.ll.Delete("item 5")
+	s.Equal(" -> item 4 -> item 3 -> item 2 -> item 1", s.ll.String())
+	s.Equal(4, s.ll.Size())
+}
+
+func (s *linkedListSuite) TestDeleteTail() {
+	s.ll.Delete("item 1")
+	s.Equal(" -> item 5 -> item 4 -> item 3 -> item 2", s.ll.String())
+	s.Equal(4, s.ll.Size())
+}
+
+func TestLinkedListSuite(t *testing.T) {
+	suite.Run(t, new(linkedListSuite))
+}
